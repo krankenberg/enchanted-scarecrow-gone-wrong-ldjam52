e638ef70a76e1b42b9002bb2caf105163d11d025
krankenberg <76975267+krankenberg@users.noreply.github.com>
2023-01-07 17:05:59 +0100

    randomize crop generation

--- Game/Field/CropManager.cs
+++ Game/Field/CropManager.cs
@@ -9,26 +9,50 @@ public partial class CropManager : Node2D
     [Export]
     private PackedScene _cropScene;
 
+    private List<Vector2> _freeCropSlots;
+    private List<Vector2> _occupiedCropSlots;
+
     private List<Crop> _crops;
 
     public override void _Ready()
     {
         RequestCropEvent.Listen(OnRequestCropEvent);
+        _freeCropSlots = new List<Vector2>();
+        _occupiedCropSlots = new List<Vector2>();
+        for (var i = 0; i < GetChildCount(); i++)
+        {
+            _freeCropSlots.Add(GetChild<Node2D>(i).GlobalPosition);
+        }
         _crops = new List<Crop>();
         CallDeferred(MethodName.SpawnCrops);
     }
 
     private void SpawnCrops()
     {
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < 20; i++)
         {
-            var position = GetChild<Node2D>(i).GlobalPosition;
+            if (_freeCropSlots.Count == 0)
+            {
+                GD.PrintErr("Couldn't find position for crop...");
+                continue;
+            }
+
+            var position = GetRandomCropSlot();
             var crop = _cropScene.Instantiate<Crop>();
             AddSibling(crop);
             crop.GlobalPosition = position;
             crop.Connect(Crop.SignalName.CropPickedUp, new Callable(this, MethodName.OnCropPickedUp));
             _crops.Add(crop);
         }
+    }
+
+    private Vector2 GetRandomCropSlot()
+    {
+        var randomIndex = Random.Generator.RandiRange(0, _freeCropSlots.Count - 1);
+        var position = _freeCropSlots[randomIndex];
+        _freeCropSlots.RemoveAt(randomIndex);
+        _occupiedCropSlots.Add(position);
+        return position;
     }
 
     private void OnCropPickedUp(Crop crop)
